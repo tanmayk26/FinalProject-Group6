6e8b96d993546e9a32c2f9f4ce7befc5891f75f0
sshikha78 <sharma07@gwu.edu>
2023-11-27 23:22:20 -0500

    Update Class_S1.py

--- Code/Class_S1.py
+++ Code/Class_S1.py
@@ -1,3 +1,20 @@
-############----
-import pandas as pd
+import os
+import re
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import nltk
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from torch import nn
+from transformers import RobertaTokenizerFast, RobertaModel
+from torch.utils.data import Dataset, DataLoader
+from torch.optim import Adam
+from tqdm import tqdm
+import argparse
+
+plt.style.use('ggplot')
+BATCH_SIZE = 32
+MAX_LEN = 128  
+model_save_name = 'trans-roberta-model.pt'
