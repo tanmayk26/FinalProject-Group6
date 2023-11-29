e3da1cea3cdd43113e5010bfa0bf59020c1fcf18
purvij07 <pjain07@gwu.edu>
2023-11-29 17:11:02 -0500

    initial preprocessing_7

--- preprocessing.py
+++ preprocessing.py
@@ -13,6 +13,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import StandardScaler
 
 # Load Spacy's English language model
 nlp = spacy.load('en_core_web_sm')
@@ -116,27 +119,102 @@ y = sarcasm_df['is_sarcastic']
 
 
 #Sentiment score
-def get_sentiment(text):
+#def get_sentiment(text):
     # Get the sentiment polarity
-    return TextBlob(text).sentiment.polarity
+    #return TextBlob(text).sentiment.polarity
 
 # Apply the function to get sentiment scores
-sarcasm_df['sentiment'] = sarcasm_df['clean_headline'].apply(get_sentiment)
+sarcasm_df['sentiment'] = sarcasm_df['clean_headline'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+# Initialize the StandardScaler
+scaler = StandardScaler(with_mean=False)  # with_mean=False to support sparse matrices
+# Scale the TF-IDF features
+X_scaled = scaler.fit_transform(X_tfidf)
 
 #Basic Model Building
 # Splitting the dataset
-X_train, X_test, y_train, y_test = train_test_split(X_tfidf, y, test_size=0.2, random_state=42)
+#X_train, X_test, y_train, y_test = train_test_split(X_tfidf, y, test_size=0.2, random_state=42)
 
-# Logistic Regression
-lr_model = LogisticRegression()
-lr_model.fit(X_train, y_train)
-y_pred_lr = lr_model.predict(X_test)
-print("Logistic Regression:")
+# Split the scaled data
+X_train_scaled, X_test_scaled, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)# Logistic Regression with GridSearchCV
+logistic_params = {'C': [0.1, 1, 10], 'solver': ['liblinear', 'lbfgs'], 'max_iter': [1000]}
+grid_lr = GridSearchCV(LogisticRegression(), logistic_params, cv=5)
+grid_lr.fit(X_train_scaled, y_train)
+
+# Fit and evaluate Logistic Regression
+grid_lr.fit(X_train_scaled, y_train)
+y_pred_lr = grid_lr.predict(X_test_scaled)
+print("Logistic Regression (Best params: {}):".format(grid_lr.best_params_))
 print(classification_report(y_test, y_pred_lr))
 
-# Naive Bayes
+# Fit and evaluate Naive Bayes
 nb_model = MultinomialNB()
-nb_model.fit(X_train, y_train)
-y_pred_nb = nb_model.predict(X_test)
+nb_model.fit(X_train_scaled, y_train)
+y_pred_nb = nb_model.predict(X_test_scaled)
 print("Naive Bayes:")
 print(classification_report(y_test, y_pred_nb))
+
+
+# Logistic Regression
+#lr_model = LogisticRegression()
+#lr_model.fit(X_train, y_train)
+#y_pred_lr = lr_model.predict(X_test)
+#print("Logistic Regression:")
+#print(classification_report(y_test, y_pred_lr))
+
+# Naive Bayes
+#nb_model = MultinomialNB()
+#nb_model.fit(X_train, y_train)
+#y_pred_nb = nb_model.predict(X_test)
+#print("Naive Bayes:")
+#print(classification_report(y_test, y_pred_nb))
+
+# Hyperparameter tuning for Logistic Regression
+#param_grid_lr = {'C': [0.1, 1, 10], 'solver': ['liblinear', 'lbfgs']}
+#grid_lr = GridSearchCV(LogisticRegression(), param_grid_lr, cv=5)
+#grid_lr.fit(X_train, y_train)
+
+# Evaluate Logistic Regression
+#y_pred_lr = grid_lr.predict(X_test)
+#print("Logistic Regression (Best params: {}):".format(grid_lr.best_params_))
+#print(classification_report(y_test, y_pred_lr))
+
+
+# Before fitting the model, updated the max_iter parameter
+#logistic_params = {'C': [0.1, 1, 10], 'solver': ['liblinear', 'lbfgs'], 'max_iter': [200]}
+
+#grid_lr = GridSearchCV(LogisticRegression(), logistic_params, cv=5)
+#grid_lr.fit(X_train, y_train)
+
+# Evaluate Logistic Regression with updated parameters
+#y_pred_lr = grid_lr.predict(X_test)
+#print("Logistic Regression (Best params: {}):".format(grid_lr.best_params_))
+#print(classification_report(y_test, y_pred_lr))
+
+# Naive Bayes Model
+#nb_model = MultinomialNB()
+#nb_model.fit(X_train, y_train)
+
+# Evaluate Naive Bayes
+#y_pred_nb = nb_model.predict(X_test)
+#print("Naive Bayes:")
+#print(classification_report(y_test, y_pred_nb))
+
+# Error Analysis
+# Analyze the misclassified examples from the models
+conf_mat_lr = confusion_matrix(y_test, y_pred_lr)
+conf_mat_nb = confusion_matrix(y_test, y_pred_nb)
+print("Confusion Matrix for Logistic Regression:\n", conf_mat_lr)
+print("Confusion Matrix for Naive Bayes:\n", conf_mat_nb)
+
+
+# Find indices of misclassified examples
+misclassified_indices = np.where(y_test != y_pred_lr)[0]
+
+# Analyze some of the misclassified examples
+for index in misclassified_indices[:10]:  # Adjust the number of examples as needed
+    print("Headline: ", sarcasm_df.iloc[index]['headline'])
+    print("Cleaned Headline: ", sarcasm_df.iloc[index]['clean_headline'])
+    print("True Label: ", "Sarcastic" if y_test[index] == 1 else "Not Sarcastic")
+    print("Predicted Label: ", "Sarcastic" if y_pred_lr[index] == 1 else "Not Sarcastic")
+    print("-" * 50)
